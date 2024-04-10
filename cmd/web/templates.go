b2fa363b0650906c630675314dc2e69b42cf3d84
mapcrafter2048 <121042282+mapcrafter2048@users.noreply.github.com>
2024-04-11 02:28:51 +0530

    done some refactoring

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -21,11 +21,11 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 	// Initialize a new map to act as cache
 
 	cache := map[string]*template.Template{}
-
-	// Use the filepath.Glob function to get a slice of all filepaths with
-	// the extension '.page.tmpl'. This essentially gives us a slice of all the
-	// 'page' templates for the application.
-
+	/*
+		Use the filepath.Glob function to get a slice of all filepaths with
+		the extension '.page.tmpl'. This essentially gives us a slice of all the
+		'page' templates for the application.
+	*/
 	pages, err := filepath.Glob(filepath.Join(dir, "*.page.tmpl"))
 	if err != nil {
 		return nil, err
